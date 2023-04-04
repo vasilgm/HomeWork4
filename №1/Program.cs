164522034b0cc50e8a3e6c01add27e911a7d1ea1
Vasil <vasilgm@yandex.ru>
2023-04-04 20:52:14 +0300

    One

--- №1/Program.cs
+++ №1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number (string N)
+{
+    int number;
+    Console.Write($"Введите число {N}: ");
+    while(!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.Write($"Введите ЧИСЛО {N}: ");
+    }
+    return number;
+}
+
+int A = number("A");
+int B = number("B");
+System.Console.WriteLine($"Число {A} в степени {B} равно {degree (A, B)}");
+
+
+int degree (int X, int Y)
+{
+    int Z = 1;
+    for (int i = 1; i <= Y; i++)
+    {
+        Z = Z * X;
+    }
+    return Z;
+}
