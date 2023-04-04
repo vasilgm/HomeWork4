60c4b60cd1e25fc3fcc038d3d950f21994b8e5f2
Vasil <vasilgm@yandex.ru>
2023-04-04 21:29:07 +0300

    two

--- №2/Program.cs
+++ №2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number (string N)
+{
+    int number;
+    System.Console.Write($"Введите число {N}: ");
+    while(!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.Write($"Введите число {N}: ");
+    }
+    return number;
+}
+
+int N = number ("N");
+System.Console.WriteLine($"Сумма цифр числа {N} равна {sum(N)}");
+
+int sum (int N)
+{
+   int remainder = 0;
+   while (N>=10)
+   {
+    remainder = remainder + N%10;
+    N = N/10;
+   }
+   return remainder+N;
+}
